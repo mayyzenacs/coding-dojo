4e73a6d91212753707aa2d8a7d7382f51a140bfe
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-11-23 16:20:19 -0300

    .

--- c/app.c
+++ c/app.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// O Conversor de Tempo Crie um programa que peça ao usuário um valor em segundos (ex: 7500) e o programa deve imprimir quanto isso vale em: Horas : Minutos : Segundos.
+
+
 int main(void) {
 
-	int a = 10;
-	int * ptr = &a;
-	int **ptrr = &ptr;
-
-	printf("ptr %d", ptr);
+	int sec;
+	printf("enter seconds");
+	scanf("%d", &sec);
 
 	return 0;
 }
