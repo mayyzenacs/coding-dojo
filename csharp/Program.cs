07057204a46651e28800064f7932681298f3fc0d
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-01-18 22:55:52 -0300

    trying to resolve a exercise

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,14 +1,47 @@
-﻿using exercises; 
+﻿using System.Data.Common;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using exercises; 
 
-namespace CursoBackend;
+namespace csharp;
 
 public class Program
 {
     // O args chega preenchido pelo SO.
     public static void Main() 
     {
-        var user = Usuario.Multiplicar(2,10);
+        var p1 = new Produto(1, "hamburguer de queijo", 29.9m);
+        var p2 = new Produto(2, "Batata", 29.9m);
+        var p3 = new Produto(3, "salada", 29.9m);
+        var p4 = new Produto(4, "Hamrbuguer sem qeuijo", 29.9m);
+        var p5 = new Produto(5, "Refrigante laranja", 29.9m);
         
-        Console.WriteLine(user);
+        var r = new Restaurante();
+
+        Console.WriteLine("bem vindo a mayyzena hotdog e tals");
+        Console.WriteLine("Escolha um numero do menu faz favor");
+        Console.WriteLine("MENU \n 1 - Fazer novo pedido \n 2- Ver pedidos anteriores \n 3 -Sair");
+        int option = int.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+            r.ListarProdutosDisponiveis();  
+            
+            int choice;
+            do
+                {
+                    Console.WriteLine("Adicione produtos pelo id, aperte 0 para sair");
+                    choice = int.Parse(Console.ReadLine());
+                }
+            while (choice > 0);
+
+            break;
+            case 2:
+            // resolver porque cadastrar produtos nao ta funcionando pra cadastrar
+            break;
+            case 3:
+            break;
+        }
     }
 }
--- csharp/exercises/Exercise.cs
+++ csharp/exercises/Exercise.cs
@@ -1,14 +1,97 @@
+using System.Runtime.ConstrainedExecution;
+
 namespace exercises; 
+
+public class Produto
+{
+    int Id;
+    string Nome;
+    decimal Valor;
+
+    public Produto(int id, string nome, decimal valor)
+    {
+        Id = id; 
+        Nome = nome;
+        Valor = valor;
+    }
+
+    public string Imprimir()
+    {
+        return string.Format($"id: {Id} nome: {Nome} Valor: {Valor}");
+    }
+
+}
+
+public class Pedido
+{
+    int Id;
+    DateTime Hora = DateTime.Now;
+    string Usuario;
+    string[] Produtos;
+
+    public void AdicionarProdutos(Produto produto)
+    {
+        
+    }
+
+    public void CalcularTotal()
+    {
+        
+    }
+
+    public void FinalizarPedido()
+    {
+        
+    }
+
+}
 
 public class Usuario  
 {
-    
-   
-    public static double Multiplicar(double Nota1, double Nota2)
+    string Nome;
+    string Telefone;
+    string[] Pedidos;
+
+    public void FazerPedido(Restaurante restaurante, Pedido pedido)
     {
-        return Nota1 / Nota2;
+        
+    }
+
+    public void VerPedidosAnteriores()
+    {
+        
     }
     
-    
-    
+
+}
+
+public class Restaurante
+{
+    string[] Produtos = new string[10];
+    string[] Lista = new string[10];
+
+
+    public static void CadastrarProduto()
+    {
+        Console.WriteLine("Digite nome produto ");
+        string newprodutoNome = Console.ReadLine();
+        Console.WriteLine("Digite preco ");
+        decimal newprodutoPreco = decimal.Parse(Console.ReadLine());
+
+        var p = new Produto(1, newprodutoNome, newprodutoPreco);
+       
+    }
+
+    public void BuscarProdutoPorId(int id)
+    {
+        
+    }
+
+    public void ListarProdutosDisponiveis()
+    {
+        foreach (var p in Produtos)
+        {
+            Console.WriteLine(p);
+        }
+    }
 }
