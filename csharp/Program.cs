60b179a46dabc18367abec4b7df7926e67031c12
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-01-23 17:17:11 -0300

    .

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,106 +1,15 @@
-﻿using System.Data.Common;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
-using exercises; 
+﻿using csharp.exercises;
 
-namespace csharp;
+namespace csharp; 
 
-public class Program
+
+class Program
 {
-    public static void Main() 
+    public static void Main()
     {
-        
-        var r = new Restaurante();
-        r.CadastrarProduto(new Produto { Id = 1, Nome = "Hamburguer", Valor = 29.9m });
-        r.CadastrarProduto(new Produto { Id = 2, Nome = "Batata frita", Valor = 14m });
-        r.CadastrarProduto(new Produto { Id = 3, Nome = "refrigerante", Valor = 14.9m });
-        r.CadastrarProduto(new Produto { Id = 4, Nome = "Nuggets", Valor = 6m });
-        r.CadastrarProduto(new Produto { Id = 5, Nome = "Batataa cheddar and baconw", Valor = 18.5m  });
-        r.CadastrarProduto(new Produto { Id = 6, Nome = "Hotdog", Valor = 20.5m  });
-        
-        Console.WriteLine("----- bem vindo a mayyzena hotdog e tals!!!! -----");
-        Console.WriteLine("Faça seu cadastro: ");
-        Console.WriteLine("---------------");
-
-        Console.WriteLine("Digite seu nome: "); 
-        string nome = Console.ReadLine();
-        Console.WriteLine("Digite seu telefone: "); 
-        string telefone = Console.ReadLine();
-        
-        var user = new Usuario(nome, telefone);
+        var e = new Executar();
+        Executar.Execution();
         
 
-        
-        int choice;
-        int option;
-
-        do
-        {
-            Console.Clear();
-            Console.WriteLine($"Olá {nome} Seja bem vindo faça um pedido");
-            Console.WriteLine("Escolha um numero do menu");
-            Console.WriteLine("MENU \n 1 - Fazer novo pedido \n 2- Ver pedidos anteriores \n 3 -Sair");
-            option = int.Parse(Console.ReadLine());
-
-
-            switch (option)
-            {
-                case 1:
-                var p = new Pedido();
-                int sair;
-
-                do {  
-                    Console.Clear();
-                    r.ListarProdutosDisponiveis();   
-
-                    Console.WriteLine("-----------");
-                    Console.WriteLine("Adicione produtos pelo id");
-                    choice = int.Parse(Console.ReadLine());
-
-                    Produto produtoEscolhido = r.BuscarProdutoPorId(choice);
-                    if (produtoEscolhido == null)
-                        {
-                            Console.Write("Produto não encontrado");
-                        } else
-                        {
-                            p.AdicionarProdutos(produtoEscolhido);
-                        }
-                    
-
-                    Console.Write("0 - Finalizar ou \n 1 - pedir mais itens "); 
-                    sair = int.Parse(Console.ReadLine());
-
-                } while (sair != 0);
-
-                if (p.produtosPedidos.Count > 0)
-                    {
-                        user.FazerPedido(p); 
-                        p.FinalizarPedido();
-                    } else
-                    {
-                        Console.Write("pedido zerado descartado");
-                    }
-
-                Console.WriteLine("\n[Pressione ENTER para voltar no menu]");
-                Console.ReadKey();
-                   
-                break;
-
-                case 2:
-                user.VerPedidosAnteriores();
-                Console.WriteLine("\n[Pressione ENTER para voltar no menu]");
-                Console.ReadKey();
-                
-                break;
-                case 3:
-                Console.Write("AGRADECEMOS POR UTILIZAR NOSSOS SERVICOS");
-                break;
-            }
-        } while (option != 3);
-        
-
-        
-   
     }
-
 }
--- csharp/exercises/Exercise.cs
+++ csharp/exercises/Exercise.cs
@@ -1,116 +1,24 @@
-namespace exercises; 
+namespace csharp.exercises; 
 
-public class Produto
+public class CalculadoraEstatica
 {
-    public int Id;
-    public string Nome;
-    public decimal Valor;
-
-}
-
-public class Pedido
-{
-    public int Id;
-    DateTime Hora = DateTime.Now;
-    public string Usuario;
-    public List<Produto> produtosPedidos = new List<Produto>();
-
-    public void AdicionarProdutos(Produto produto)
+    public static int Somar(int a, int b)
     {
-        produtosPedidos.Add(produto);
-        Console.WriteLine($"adicionado ao pedido: {produto.Nome}");
+        return a +b;
     }
 
-    public string CalcularTotal()
+    public static int Multiplicar(int a, int b)
     {
-        return string.Format($"{produtosPedidos.Sum(produtosPedidos => produtosPedidos.Valor)}");
-    }
-
-    public void FinalizarPedido()
-    {
-        Console.WriteLine($"finalizando pedido para {Usuario}");
-        Console.WriteLine("-----------");
-        foreach (var p in produtosPedidos)
-        {
-            Console.WriteLine($"---- Produto {p.Nome} Valor {p.Valor} ----");
-        }
-        Console.WriteLine($"Total do pedido: {CalcularTotal()}");
-        Console.WriteLine("-----------");
-        Console.WriteLine($"Horario do pedido {Hora} seu pedido estára pronto em BREVE VOLTE SEMPRE :)");
+        return a * b;
     }
 }
 
-public class Usuario  
+public class Executar
 {
-    public string Nome;
-    string Telefone;
-    private List<Pedido> pedidosRealizados = new List<Pedido>();
-
-    public Usuario(string nome, string telefone)
+    public static void Execution()
     {
-        Nome = nome;
-        Telefone = telefone;
-    }
+        var r = CalculadoraEstatica.Multiplicar(2, 5);
 
-    public void FazerPedido(Pedido pedido)
-    {
-        var rnd = new Random(); 
-        pedido.Id = rnd.Next();
-
-        pedido.Usuario = this.Nome;
-        pedidosRealizados.Add(pedido);
-        
-    }
-
-    public void VerPedidosAnteriores()
-    {
-        foreach (var pedido in pedidosRealizados)
-        {
-            foreach (var produto in pedido.produtosPedidos)
-            {
-                Console.WriteLine($"Pedido: {produto.Nome} Valor: {produto.Valor}");
-            }
-            Console.WriteLine($"Total do pedido: {pedido.CalcularTotal()}");
-        }
-    }
-}
-
-public class Restaurante
-{
-    public List<Pedido> listaPedidos = new List<Pedido>();
-    List<Produto> produtos = new List<Produto>();
-
-    public void CadastrarProduto(Produto produto)
-    {
-        foreach (var p in produtos)
-        {
-            if (p.Id == produto.Id)
-            {
-                Console.WriteLine($"Erro ja existe um produto com esse ID:  {produto.Id}");
-                return; 
-            }
-        }
-        produtos.Add(produto);
-    }
-
-    public Produto BuscarProdutoPorId(int id)
-    {
-        foreach (var p in produtos)
-        {
-            if (p.Id == id)
-            {
-                return p; 
-            }   
-        } 
-      
-        return null;
-    }
-
-    public void ListarProdutosDisponiveis()
-    {
-        foreach(var p in produtos)
-        {
-            Console.WriteLine($"id: {p.Id} nome: {p.Nome} Valor: {p.Valor}");
-        }
+        Console.WriteLine(CalculadoraEstatica.Somar(2,5));
     }
 }
