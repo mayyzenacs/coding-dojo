b5ae9a07d5e85ccf8c521077767db1c03e840a83
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-01-27 20:43:24 -0300

    .

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,23 @@
-const b1 = [23,45,64,21]
-const b2 = [3,45,68,90,44,56,123,34]
+function convertFormat(usd, brl) {
+  if (isNaN(usd) || isNaN(brl) || brl <= 10) {
+    return 0;
+  }
+
+  const convertion = usd * brl;
+
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(convertion);
+}
+
+const usd = document.getElementById("usd-value");
+const brl = document.getElementById("exchange-rate");
+const button = document.getElementById("convert-btn");
+const resultLabel = document.getElementById("result-value");
+
+button.addEventListener("click", () => {
+  const result = convertFormat(usd.value, brl.value);
+
+  resultLabel.innerHTML = result;
+});
