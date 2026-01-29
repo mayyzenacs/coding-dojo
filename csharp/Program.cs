ad2c5350fb143677312fc9c05a93497c8c52a757
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-01-29 17:52:03 -0300

    learned new methods in C#.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,12 +1,34 @@
-﻿using csharp.exercises;
+﻿public class Validador
+{
+    
+    public bool TentarConverter(string texto, out int resultado)
+    {
+        if (int.TryParse(texto, out int numeroConvertido))
+        {
+            resultado = numeroConvertido; // 
+            return true;
+        }
 
-namespace csharp; 
+        resultado = 0; 
+        return false;
+    }
+}
 
-public class Program { 
-
+public class Program
+{
     public static void Main()
     {
-        
-    }
+        Validador val = new Validador();
+        string input = "123";
 
+        
+        if (val.TentarConverter(input, out int numero))
+        {
+            Console.WriteLine($"Sucesso: {numero}");
+        }
+        else
+        {
+            Console.WriteLine("Não é um número.");
+        }
+    }
 }
--- csharp/exercises/Exercise.cs
+++ csharp/exercises/Exercise.cs
@@ -3,10 +3,7 @@ namespace csharp.exercises;
 
 public struct Pessoa
 {
-    public int Idade { get; set; }
-    public string Nome { get; set; }
-    public string Cpf { get; set; }
-    public string Email { get; set; }
+   
 
 }
 
