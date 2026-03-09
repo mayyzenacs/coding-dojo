a1167082f549b7290c2b904a255da463e2ed80ab
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-03-09 11:50:19 -0300

    TIL: Fortification in Inheritance

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,11 +1,17 @@
-﻿namespace csharp;
+﻿using csharp.exercises;
+
+namespace csharp;
+
+
 
 public class Program
 {
     public static void Main()
     {
+            var Feio = new CarroFeio("Fiat", 10, "saveiro", 4);
+            Console.WriteLine(Feio.Acelerar(2));
+            
 
-
-        Console.WriteLine(filmes.Keys);
+        
     }
 }
--- csharp/exercises/Exercise.cs
+++ csharp/exercises/Exercise.cs
@@ -1,11 +1,28 @@
 
 namespace csharp.exercises; 
 
-public struct Pessoa
+public class Carro(string nome, int velocidade, string modelo)
 {
-   
+    protected string Nome = nome;
+    protected int Velocidade = velocidade;
+    public string Modelo = modelo;
 
+    public int Acelerar(int kmh)
+    {
+        if (kmh == 0) kmh = 0;
+
+        if (kmh > 0) kmh += 5;
+
+        return kmh;
+    }
 }
 
+public class CarroFeio(string nome, int velocidade, string modelo, int rodas) : Carro(nome, velocidade, modelo)
+{
+    public string Desc()
+    {
+        return $"carro feio {Nome} anda {Velocidade} e tem {rodas}"; 
+    }
+}
 
 
