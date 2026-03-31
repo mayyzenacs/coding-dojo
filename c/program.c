9bfff81efcb30e139cde92ba1bc2d79d28442da1
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-03-31 08:08:37 -0300

    TIL c structers

--- c/program.c
+++ c/program.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// O Conversor de Tempo Crie um programa que peça ao usuário um valor em segundos (ex: 7500) e o programa deve imprimir quanto isso vale em: Horas : Minutos : Segundos.
 
 
 int main(void) {
 
+	int a = 10; 
+	int *p1 = &a;
+	int **p2 = &p1;
+	
+	printf("a: %d, &a = %p\n", a, &a);
+	printf("p1: %d, &p1 = %p", p1, &p1);
 
-
-	int seconds_entry;
-	printf("Digite os segundos: "); 
-	if (scanf("%d", &seconds_entry) != 1) {
-		printf("Digite um numero válido");
-		return 1;
-	}
-
-	int horas = seconds_entry / 3600;
-	int minutos = (seconds_entry % 3600) / 60;
-	int seconds = (seconds_entry % 3600) % 60;
-	printf("%02d Horas %02d Minutos %02d Segundos\n", horas, minutos, seconds);
-
+	
 
 	return 0;
 }
