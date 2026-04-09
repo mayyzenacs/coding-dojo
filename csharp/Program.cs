b936e038dc59382eef37b2183442a8cb2c78f223
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-04-09 17:58:48 -0300

    TIL csharp abstract class

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -8,8 +8,6 @@ public class Program
 {
     public static void Main()
     {
-            var Feio = new CarroFeio("Fiat", 10, "saveiro", 4);
-            Console.WriteLine(Carro.Acelerar(2));
             
 
         
--- csharp/exercises/Exercise.cs
+++ csharp/exercises/Exercise.cs
@@ -1,32 +1,17 @@
 
 namespace csharp.exercises; 
 
-public class Carro(string nome, int velocidade, string modelo)
+public abstract class Videoagame
 {
-    protected string Nome = nome;
-    protected int Velocidade = velocidade;
-    public string Modelo = modelo;
+    public abstract string Nome();
 
-    public virtual int Acelerar(int kmh)
-    {
-        if (kmh == 0) kmh = 0;
-
-        if (kmh > 0) kmh += 5;
-
-        return kmh;
-    }
 }
 
-public class CarroFeio(string nome, int velocidade, string modelo, int rodas) : Carro(nome, velocidade, modelo)
+public class Playstation : Videoagame
 {
-    public string Desc()
+    public override string Nome()
     {
-        return $"carro feio {Nome} anda {Velocidade} e tem {rodas}"; 
-    }
-
-    public override int Acelerar(int kmh)
-    {
-        return base.Acelerar(kmh);
+        return "ola, sou da playstation";
     }
 }
 
